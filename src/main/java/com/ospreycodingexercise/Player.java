5f2987fcc54b7011cfbca201561b38f8a097c3d1
Piers Beckley <piers@fatpigeons.com>
2022-08-13 17:16:53 +0100

    Direction can be changed

--- src/main/java/com/ospreycodingexercise/Player.java
+++ src/main/java/com/ospreycodingexercise/Player.java
@@ -11,19 +11,21 @@ public class Player {
     @GeneratedValue
     private Long id;
     
-    private int x;
-    private int y;
+    private Integer x;
+    private Integer y;
     
     private String direction;
 
     public void setX(int x) {
-        if (x <= 10) {
+        if ( x >= 1 && x <= 10) {
             this.x = x;
         }
     }
     
     public void setY(int y) {
-        this.y = y;
+        if (y >= 1 && y <= 10 ) {
+            this.y = y;
+        }
     }
 
     public String getLocation() {
--- src/test/java/com/ospreycodingexercise/PlayerTest.java
+++ src/test/java/com/ospreycodingexercise/PlayerTest.java
@@ -1,5 +1,8 @@
 package com.ospreycodingexercise;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -8,20 +11,48 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class PlayerTest {
-
-
+    private Player player;
+    
+    @BeforeEach
+    public void before() throws Exception{
+        player = new Player();
+    }
+    
+    @AfterEach
+    public void after() throws Exception {
+        player = null;
+    }
+    
     @ParameterizedTest
     @MethodSource(value = "provideOutOfBoundsLocations")
     public void LocationCannotBeSetOutOfBounds(int[] location) {
-        Player player = new Player();
         int x = location[0];
         int y = location[1];
-        assertThat(player.getLocation()).isNotEqualTo(x + "x" + y)
-                .withFailMessage("A co-ordinate is in bounds and shouldn't be. X:" + x + " Y:" + y);
+        player.setX(x);
+        player.setY(y);
+        assertThat(player.getLocation())
+                .as("Setting x: " + x +" y:" + y + " should have been rejected due to exceeding board boundaries")
+                .isNotEqualTo(x + "x" + y);
     }
 
     public static int[][] provideOutOfBoundsLocations() {
-        return new int[][]{{20, 5}};
+        return new int[][]{
+                {20, 5}, // x high
+                {0, 5}, // x low
+                {-5, 5}, // x negative
+                {5, 20}, // y high
+                {5, 0}, // y low
+                {5, -5}, // y negative
+        };
     }
+    
+    @Test
+    public void DirectionCanBeChanged() {
+        player.setDirection("NORTH");
+        assertThat(player.getDirection()).isEqualTo("NORTH");
+        player.setDirection("WEST");
+        assertThat(player.getDirection()).isEqualTo("WEST");
+    }
+    
 
 }
