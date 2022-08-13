5bdbf07a510c45fc20001c4077ce876ee5d324f5
Piers Beckley <piers@fatpigeons.com>
2022-08-13 19:15:54 +0100

    End of day commit

--- src/main/java/com/ospreycodingexercise/PlayerController.java
+++ src/main/java/com/ospreycodingexercise/PlayerController.java
@@ -5,9 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.cert.X509Certificate;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Random;
 
 @RestController
@@ -24,19 +21,17 @@ public class PlayerController {
     
     @GetMapping("/api/reset")
     public String resetBoard() {
-//        Random random = new Random();
-//        int x = random.nextInt(10) + 1;
-//        int y = random.nextInt(10) + 1;
-//        List<String> directionList = Arrays.asList("NORTH", "SOUTH", "EAST", "WEST");
-//        String direction = directionList.get(random.nextInt(directionList.size()));
-//        Player player = new Player();
-//        player.setX(x);
-//        player.setY(y);
-//        player.setDirection(direction);
+        Random random = new Random();
+        int x = random.nextInt(10) + 1;
+        int y = random.nextInt(10) + 1;
+        int directionNumber = random.nextInt(4);
+        String[] directionList = {"NORTH", "SOUTH", "EAST", "WEST"};
+        String direction = directionList[directionNumber];
+        
         Player player = new Player();
-        player.setX(5);
-        player.setY(5);
-        player.setDirection("NORTH");
+        player.setX(x);
+        player.setY(y);
+        player.setDirection(direction);
         playerRepository.save(player);
         return "Reset board!";
     }
