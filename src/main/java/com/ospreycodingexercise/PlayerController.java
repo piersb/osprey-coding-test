0925d24dccd83888c8dd95faac3f9834c86f4645
Piers Beckley <piers@fatpigeons.com>
2022-08-13 17:42:53 +0100

    Better db setup (in that it actually, uh, works...)

--- src/main/java/com/ospreycodingexercise/PlayerController.java
+++ src/main/java/com/ospreycodingexercise/PlayerController.java
@@ -1,7 +1,6 @@
 package com.ospreycodingexercise;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,15 +8,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PlayerController {
     
+    @Autowired
+    private PlayerRepository playerRepository;
+    
     @GetMapping("/api/board")
     @ResponseBody
-    public String getPlayerInformation() throws JsonProcessingException {
-        Player player = new Player();
-        player.setX(2);
-        player.setY(2); 
-        player.setDirection("NORTH");
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.writeValueAsString(player);
+    public Iterable<Player> getPlayerInformation() {
+        return playerRepository.findAll();
     }
     
 }
---
+++ src/main/java/com/ospreycodingexercise/PlayerRepository.java
@@ -0,0 +1,6 @@
+package com.ospreycodingexercise;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PlayerRepository extends CrudRepository<Player, Long> {
+}
