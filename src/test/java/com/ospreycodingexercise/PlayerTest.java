a8aee93dc6cacfa24513020b4a5ab3b256e269ca
Piers Beckley <piers@fatpigeons.com>
2022-08-13 16:29:57 +0100

    Better errors

--- src/test/java/com/ospreycodingexercise/PlayerTest.java
+++ src/test/java/com/ospreycodingexercise/PlayerTest.java
@@ -9,18 +9,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 public class PlayerTest {
 
-    
+
     @ParameterizedTest
     @MethodSource(value = "provideOutOfBoundsLocations")
     public void LocationCannotBeSetOutOfBounds(int[] location) {
         Player player = new Player();
         int x = location[0];
         int y = location[1];
-        assertThat(player.getLocation()).isNotEqualTo(x + "x" + y);
+        assertThat(player.getLocation()).isNotEqualTo(x + "x" + y)
+                .withFailMessage("A co-ordinate is in bounds and shouldn't be. X:" + x + " Y:" + y);
     }
-    
+
     public static int[][] provideOutOfBoundsLocations() {
-        return new int[][] {{20, 5}};
+        return new int[][]{{20, 5}};
     }
 
 }
