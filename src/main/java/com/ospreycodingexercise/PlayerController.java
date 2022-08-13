0d84021bef0cdb2432824113ae26bb74ad2a95fc
Piers Beckley <piers@fatpigeons.com>
2022-08-13 19:06:22 +0100

    Reset board not 500ing!

--- src/main/java/com/ospreycodingexercise/PlayerController.java
+++ src/main/java/com/ospreycodingexercise/PlayerController.java
@@ -5,6 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.cert.X509Certificate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
 @RestController
 public class PlayerController {
     
@@ -15,6 +20,25 @@ public class PlayerController {
     @ResponseBody
     public Iterable<Player> getPlayerInformation() {
         return playerRepository.findAll();
+    }
+    
+    @GetMapping("/api/reset")
+    public String resetBoard() {
+//        Random random = new Random();
+//        int x = random.nextInt(10) + 1;
+//        int y = random.nextInt(10) + 1;
+//        List<String> directionList = Arrays.asList("NORTH", "SOUTH", "EAST", "WEST");
+//        String direction = directionList.get(random.nextInt(directionList.size()));
+//        Player player = new Player();
+//        player.setX(x);
+//        player.setY(y);
+//        player.setDirection(direction);
+        Player player = new Player();
+        player.setX(5);
+        player.setY(5);
+        player.setDirection("NORTH");
+        playerRepository.save(player);
+        return "Reset board!";
     }
     
 }
--- src/main/java/com/ospreycodingexercise/PlayerRepository.java
+++ src/main/java/com/ospreycodingexercise/PlayerRepository.java
@@ -3,4 +3,5 @@ package com.ospreycodingexercise;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PlayerRepository extends CrudRepository<Player, Long> {
+    Player findPlayerById(Integer id);
 }
