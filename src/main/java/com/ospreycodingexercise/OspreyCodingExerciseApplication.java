8d50339586a8c9aa6ce399c07cacf1b02df66582
Piers Beckley <piers@fatpigeons.com>
2022-08-14 19:09:06 +0100

    Better error handling

--- src/main/java/com/ospreycodingexercise/OspreyCodingExerciseApplication.java
+++ src/main/java/com/ospreycodingexercise/OspreyCodingExerciseApplication.java
@@ -2,8 +2,9 @@ package com.ospreycodingexercise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {ErrorMvcAutoConfiguration.class})
 public class OspreyCodingExerciseApplication {
 
     public static void main(String[] args) {
