2bfb11d250dff38e2879cc84072fc666204bd4fd
Piers Beckley <piers@fatpigeons.com>
2022-08-14 12:31:34 +0100

    stash

--- src/main/java/com/ospreycodingexercise/AvatarRepository.java
+++ src/main/java/com/ospreycodingexercise/AvatarRepository.java
@@ -3,6 +3,7 @@ package com.ospreycodingexercise;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AvatarRepository extends CrudRepository<Avatar, Long> {
     Avatar findAvatarById(Integer id);
 }
